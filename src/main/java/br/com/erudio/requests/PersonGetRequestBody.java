f60dbc96897ebbc80fdb893b47d7ba5a39194697
Felix-Linux <rodrigofelixferreira@hotmail.com>
2024-02-24 17:29:59 -0300

    final section 12 custom json

--- src/main/java/br/com/erudio/requests/PersonGetRequestBody.java
+++ src/main/java/br/com/erudio/requests/PersonGetRequestBody.java
@@ -1,25 +1,29 @@
 package br.com.erudio.requests;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @Builder
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonGetRequestBody {
 
     private Long id;
 
-
+    @JsonProperty("first_name")
     private String firstName;
 
-
+    @JsonProperty("last_name")
     private String lastName;
 
 
     private String address;
 
 
+    @JsonIgnore
     private String gender;
 }
