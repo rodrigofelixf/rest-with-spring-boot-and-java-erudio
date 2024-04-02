dcdac04c4be4bb42e1587c5fa07526a7daf8511a
Rodrigo Felix <111197337+rodrigofelixf@users.noreply.github.com>
2024-04-02 12:43:52 -0300

    Update StartupTests.java

--- src/test/java/br/com/erudio/StartupTests.java
+++ src/test/java/br/com/erudio/StartupTests.java
@@ -1,13 +1,13 @@
-package br.com.erudio;
+// package br.com.erudio;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class StartupTests {
+// @SpringBootTest
+// class StartupTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
